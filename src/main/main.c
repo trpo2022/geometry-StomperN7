f85fe7ed982b7806d17ff019072db8238d488f9d
StomperN7 <imnotcrabsnub1@gmail.com>
2022-03-16 17:27:06 +0600

    Add src files

---
+++ src/main/main.c
@@ -0,0 +1,21 @@
+#include "libgeo/circle.h"
+#include "libgeo/poligon.h"
+#include "libgeo/triangle.h"
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	printf("1. Cirle\n2. Triangle\nChoose your figure: ");
+	scanf(" %d", &a);
+	if (a == 1) 
+	{
+		circle();
+	}
+	if (a == 2) 
+	{
+		triangle();
+	}
+	return 0;
+}
